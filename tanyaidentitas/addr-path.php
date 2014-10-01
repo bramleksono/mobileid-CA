6f817e0dc365533d893c47bb9dfa8a9a82136840
adriandwiananto <bangateng@yahoo.com>
2014-10-01 19:51:05 +0700

    move address var to other file

---
+++ tanyaidentitas/addr-path.php
@@ -0,0 +1,6 @@
+<?php
+$CAcallbackaddr = "http://localhost/ca/mobileid-CA/tanyaidentitas/terimakonfirmasiCA.php";
+// $CAcallbackaddr = "http://red-trigger-44-141737.apse1.nitrousbox.com/CA/tanyaidentitas/terimakonfirmasiCA.php";
+$SIaddr = "http://localhost/si/mobileid-SI/daftarlogin.php";
+// $SIaddr = "http://red-trigger-44-141737.apse1.nitrousbox.com/SI/daftarlogin.php";
+?>
--- tanyaidentitas/daftarrequestCA.php
+++ tanyaidentitas/daftarrequestCA.php
@@ -1,11 +1,12 @@
 <?php
 require_once('../lib/filemanipulation.php');
+include ('./addr-path.php');
 
 //konfigurasi
 $CAid = "123123123asfgfjhhtweasdvbgjtyrewras";
-$CAcallbackaddr = "http://localhost/ca/mobileid-CA/tanyaidentitas/terimakonfirmasiCA.php";
+// $CAcallbackaddr = "http://localhost/ca/mobileid-CA/tanyaidentitas/terimakonfirmasiCA.php";
 // $CAcallbackaddr = "http://red-trigger-44-141737.apse1.nitrousbox.com/CA/tanyaidentitas/terimakonfirmasiCA.php";
-$SIaddr = "http://localhost/si/mobileid-SI/daftarlogin.php";
+// $SIaddr = "http://localhost/si/mobileid-SI/daftarlogin.php";
 // $SIaddr = "http://red-trigger-44-141737.apse1.nitrousbox.com/SI/daftarlogin.php";
 //
 
